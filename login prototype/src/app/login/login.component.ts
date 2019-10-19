3606bcde3958e0f2344d2904687d9d0630677d7a
Dominik Ummel <dominik.ummel@students.unibe.ch>
2019-10-19 12:16:46 +0200

    Added basic methods to logincomponent

--- login prototype/src/app/login/login.component.ts
+++ login prototype/src/app/login/login.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+/**
+ * Login field for customers
+ */
+
+import { Component, OnInit, Input } from '@angular/core';
+import { DatabaseComponent } from "../database/database.component";
+import { DatabaseService } from "../database/database.service";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-login',
@@ -6,8 +13,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  error = null;
+  customer = {};
+  database_url = "http://localhost:3001/account/";
+  database = new DatabaseService(this.http, this.database_url);
 
-  constructor() { }
+  login(name: string, password: string,) {
+    if (!this.database.userExists(name))
+      this.error = "User doesn't exist";
+    if (!this.database.passwordMatches(password))
+      this.error = "Password is not correct";
+    else {
+      this.error = "Login successful";
+    }
+  }
+
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
