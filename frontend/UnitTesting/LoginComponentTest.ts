e9168b66654e4c55519b734f13a1f0ebb14cdd37
Dominik Ummel <dominik.ummel@students.unibe.ch>
2019-11-11 16:58:34 +0100

    Started to implemnt testing for Login Component

---
+++ frontend/UnitTesting/LoginComponentTest.ts
@@ -0,0 +1,16 @@
+import {LoginComponent} from '../src/app/login/login.component';
+import {TestBed, async} from '@angular/core/testing';
+
+/*beforeEach(async (() =>{
+  TestBed.configureTestingModule;
+  declarations: [
+    LoginComponent
+    ]
+  imports: [
+
+    ]
+  providers: [
+
+  ]
+  }).compileComponets();*/
+
