8a1d24996541f1642201b26fd3bbfe3752226299
Dominik Ummel <dominik.ummel@students.unibe.ch>
2019-11-13 10:08:25 +0100

    Fixed more tests, All but DatabseComponent running

--- frontend/src/app/login/login.service.spec.ts
+++ frontend/src/app/login/login.service.spec.ts
@@ -1,9 +1,28 @@
 import { TestBed } from '@angular/core/testing';
 
 import { LoginService } from './login.service';
+import {DatabaseService} from '../database/database.service';
+import {HttpClientModule} from '@angular/common/http';
+
+/**
+ * A mock class to test LoginService independent from DatabaseService
+ */
+class MockDatabaseService extends DatabaseService {
+  add() {}
+  post() {}
+}
 
 describe('LoginService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientModule
+    ],
+    providers: [
+      {provide: DatabaseService,
+      useClass: MockDatabaseService},
+      {provide: String, useValue: 'abc'}
+    ]
+  }));
 
   it('should be created', () => {
     const service: LoginService = TestBed.get(LoginService);
