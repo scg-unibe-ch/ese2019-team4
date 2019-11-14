376cec65d9070e0468f118dd96ae5e4a0d08ee01
Johannes Meier <johanes_meier@outlook.com>
2019-11-14 01:16:21 +0100

    Changed routing for registration

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,17 +5,8 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './pages/home/home.module#HomePageModule' },
   {
-    path: 'login-user',
-    children: [
-      {
-        path: '',
-        loadChildren: './pages/login-user/login-user.module#LoginUserPageModule'
-      },
-      {
-        path: 'register-consumer', loadChildren: './pages/login-user/register-consumer/register-consumer.module#RegisterConsumerPageModule'
-      }
-    ]
-  },
+    path: 'login-user', loadChildren: './pages/login-user/login-user.module#LoginUserPageModule'},
+  { path: 'register', loadChildren: './pages/register-consumer/register-consumer.module#RegisterConsumerPageModule'},
   { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' }
 
 ];
