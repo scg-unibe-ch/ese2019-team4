aecb475bab247432f554791c9f84e77113afd0b5
Anton Park <anton.park@students.unibe.ch>
2019-11-17 19:15:18 +0100

    cleaned up profile.ts and home.ts

--- frontend/src/app/pages/home/home.page.ts
+++ frontend/src/app/pages/home/home.page.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, OnChanges, SimpleChanges} from '@angular/core';
+import {Component, OnInit, SimpleChanges} from '@angular/core';
 import {Post} from '../../services/post/post.model';
 import { PostService} from '../../services/post/post.service';
 import {HttpClient} from '@angular/common/http';
@@ -10,7 +10,7 @@ import { SessionService } from '../../services/session.service';
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit, OnChanges {
+export class HomePage implements OnInit {
   posts = [];
   username = this.session.info.username;
   type = this.session.info.type;
@@ -19,29 +19,10 @@ export class HomePage implements OnInit, OnChanges {
 
   constructor(private postService: PostService, private http: HttpClient, public session:SessionService) {
   }
-  loggedInStatus() {
-    if (this.username === null) {
-      this.status = 'Logged out';
-    }
-    else {
-      this.status = 'Logged in as ' + this.username + ' a ' + this.type;
-    }
-  }
 
   ngOnInit() {
     this.postService.getPosts().subscribe(data => {
       this.posts = data['instances'];
     });
-    this.loggedInStatus();
     }
-
-  updateUser() {
-    this.username = this.session.info.username;
-    this.type = this.session.info.type;
-    this.loggedInStatus();
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    this.updateUser();
-  }
 }
