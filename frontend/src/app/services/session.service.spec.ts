d38c2dc460de76579b16edc1a654789a7e5b723c
domibwagz <55526433+domibwagz@users.noreply.github.com>
2019-11-26 10:28:51 +0100

    Improved Tests for all Pages

--- frontend/src/app/services/session.service.spec.ts
+++ frontend/src/app/services/session.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SessionService } from './session.service';
+import {HttpClientModule} from '@angular/common/http';
 
 describe('SessionService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientModule
+    ],
+  }));
 
   it('should be created', () => {
     const service: SessionService = TestBed.get(SessionService);
